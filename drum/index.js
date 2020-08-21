da4fc7b9f4e03a37e12a3f0974ee8aabf7fa671e
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-08-20 20:26:31 -0700

    create clock animation w/ vanilla js

--- drum/index.js
+++ drum/index.js
@@ -9,7 +9,6 @@ window.addEventListener('keydown', function(e) {
 });
 
 function removeTransition(e) {
-  console.log(e)
   if (e.propertyName !== 'transform') return;
   this.classList.remove('playing')
 }
